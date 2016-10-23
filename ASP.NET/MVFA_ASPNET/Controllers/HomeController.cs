0278a97bca2816f45ee169c1b4010576d500cc4a
Nick Gilbert <nickgilbert1994@gmail.com>
2016-10-23 15:33:39 -0400

    Account and Initial Database Setup now operational

--- ASP.NET/MVFA_ASPNET/Controllers/HomeController.cs
+++ ASP.NET/MVFA_ASPNET/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace MVFA_ASPNET.Controllers
 {
@@ -10,12 +14,24 @@ namespace MVFA_ASPNET.Controllers
     {
         public ActionResult Index()
         {
+            Console.WriteLine("Loading Home page");
             return View();
         }
 
         public ActionResult Setup()
         {
             return View();
+        }
+
+        public ActionResult GenerateDatabase()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            MySqlCommand cmd = new MySqlCommand("Generate_ASPNET_Database", new MySqlConnection(constr));
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+            return RedirectToAction("Register", "Account");
         }
     }
 }
