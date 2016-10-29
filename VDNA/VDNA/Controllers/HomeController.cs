38802fbf3b918ac4f89c175a7809efbdfc17f386
Nick Gilbert <nickgilbert1994@gmail.com>
2016-10-29 17:05:20 -0400

    Added GenerateDB Functionality, Views

--- VDNA/VDNA/Controllers/HomeController.cs
+++ VDNA/VDNA/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VDNA.Models;
 
 namespace VDNA.Controllers
 {
@@ -13,8 +18,60 @@ namespace VDNA.Controllers
             return View();
         }
 
+        public ActionResult Instructions()
+        {
+            return View();
+        }
+
+        public ActionResult ResetDatabase()
+        {
+            return View();
+        }
+
+        public ActionResult XSSAttack()
+        {
+            return View();
+        }
+
+        public ActionResult SQLInject()
+        {
+            return View();
+        }
+
+        public ActionResult BruteForce()
+        {
+            return View();
+        }
+
+        public ActionResult Settings()
+        {
+            return View();
+        }
+
         public ActionResult GenerateDatabase()
         {
+            //Create Empty Database
+            var myDbContext = new ApplicationDbContext();
+            myDbContext.Database.Delete();
+            myDbContext.Database.Create();
+
+            //Populate tables with initial data
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(myDbContext));
+            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(myDbContext));
+
+            // Create Admin Role
+            RoleManager.Create(new IdentityRole("Admin"));
+            RoleManager.Create(new IdentityRole("User"));
+
+            //Creating Admin Account
+            PasswordHasher hasher = new PasswordHasher();
+            ApplicationUser adminUser = new ApplicationUser();
+            adminUser.Email = "admin@test.com";
+            adminUser.EmailConfirmed = true;
+            adminUser.PasswordHash = hasher.HashPassword("mockingjay");
+            adminUser.UserName = "admin@test.com";
+            UserManager.Create(adminUser);
+
             return RedirectToAction("Register", "Account");
         }
     }
